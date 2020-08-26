396cfc1371dfe4c1f69c89f72fc12cd47fb66924
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2020-08-26 00:52:36 -0300

    base CLI pakcage

---
+++ cli/bot/commands.go
@@ -0,0 +1,44 @@
+package bot
+
+import (
+	"github.com/RafilxTenfen/go-chat/cli"
+	"github.com/ibraimgm/libcmd"
+	"github.com/rhizomplatform/fs"
+	"github.com/rhizomplatform/log"
+)
+
+// terminalHandler exists only to "pass" the
+// cli.Terminal along without requiring a global variable and
+type terminalHandler struct {
+	term cli.Terminal
+}
+
+// Main handles all command line interfaces
+func Main(args []string) int {
+	h := &terminalHandler{cli.Terminal{}}
+	app := libcmd.NewApp("Bot Service", "Bot that reads and publish into RabbitMQ queues")
+	app.Options.HelpOutput = h.term.Out()
+	app.Options.StrictOperands = true
+
+	// loggin setup
+	log.Setup(fs.Path("./log/bot"), "bot service", 30, 60)
+	defer log.TearDown()
+	log.SetStdoutLevel(log.LevelDebug)
+
+	app.Command("run", "Run the bot service", func(cmd *libcmd.Cmd) {
+		cmd.Run(h.run)
+	})
+
+	// root - no command specified
+	app.Run(func(cmd *libcmd.Cmd) error {
+		app.PrintHelp(h.term.Out())
+		return nil
+	})
+
+	if err := app.ParseArgs(args); err != nil {
+		h.term.Printf("%v\n", cli.UserError(err))
+		return 1
+	}
+
+	return 0
+}
---
+++ cli/bot/run.go
@@ -0,0 +1,9 @@
+package bot
+
+import (
+	"github.com/ibraimgm/libcmd"
+)
+
+func (t *terminalHandler) run(cmd *libcmd.Cmd) error {
+	return nil
+}
