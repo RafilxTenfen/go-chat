3153762137cd3e676027112a28bc0e42a7452517
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2020-08-27 01:29:43 -0300

    add NewBot that creates a new Bot Structure

--- robot/bot.go
+++ robot/bot.go
@@ -1,7 +1,41 @@
 package robot
 
+import (
+	"github.com/RafilxTenfen/go-chat/rabbit"
+	"github.com/streadway/amqp"
+)
+
 // Bot represents a Robot to listen and publish at N queues
 type Bot struct {
-	queues   map[string]uint16
-	settings Settings
+	queueMap   *rabbit.QueueMap
+	connection *amqp.Connection
+	chanel     *amqp.Channel
+	settings   Settings
+}
+
+// NewBot returns a new Bot structure
+func NewBot(settings Settings, queuesName []string) (*Bot, error) {
+
+	conn, ch, err := rabbit.Init(settings.RabbitMqURL)
+	if err != nil {
+		return nil, err
+	}
+
+	queues := rabbit.NewQueueMap()
+	for i := range queuesName {
+		name := queuesName[i]
+		queue := rabbit.NewQueue(name)
+
+		if err := queue.Declare(ch); err != nil {
+			return nil, err
+		}
+		queues.Store(queue)
+	}
+
+	return &Bot{
+		settings:   settings,
+		queueMap:   queues,
+		connection: conn,
+		chanel:     ch,
+	}, err
 }
