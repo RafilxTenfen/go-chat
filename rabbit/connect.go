3bfbb811c8f9c7c98f7a826e3094b165740bf517
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2020-08-27 01:28:09 -0300

    init of rabbit package

---
+++ rabbit/connect.go
@@ -0,0 +1,20 @@
+package rabbit
+
+import (
+	"github.com/streadway/amqp"
+)
+
+// Init starts a connection with rabbitMQ
+func Init(url string) (*amqp.Connection, *amqp.Channel, error) {
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return conn, ch, nil
+}
