37e235b9ced080019f0c1be4dd528dd90c944499
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2020-08-28 00:39:32 -0300

    add read password method in terminal struct

--- cli/terminal.go
+++ cli/terminal.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/signal"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 // Terminal implements terminal stdout
@@ -22,4 +26,33 @@ func (*Terminal) In() io.Reader {
 // Printf use fmt printf
 func (*Terminal) Printf(format string, args ...interface{}) {
 	fmt.Printf(format, args...)
+}
+
+// ReadPassword reads a password from terminal
+func (t *Terminal) ReadPassword() string {
+	// initial state of the terminal
+	inittermstate, err := terminal.GetState(syscall.Stdin)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// restore it in the event of an interrupt
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		_ = terminal.Restore(syscall.Stdin, inittermstate)
+		os.Exit(1)
+	}()
+
+	// getpass
+	p, err := terminal.ReadPassword(syscall.Stdin)
+	fmt.Println("")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// looking for ^C
+	signal.Stop(c)
+
+	return string(p)
 }
