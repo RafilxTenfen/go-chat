76bde14013896d31b44aa7f487e4b43efc76b18a
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2020-08-28 23:23:29 -0300

    add print queues to bot shell

--- robot/shell.go
+++ robot/shell.go
@@ -3,7 +3,6 @@ package robot
 import (
 	"os"
 
-	"github.com/RafilxTenfen/go-chat/rabbit"
 	"github.com/rhizomplatform/log"
 	"gopkg.in/abiosoft/ishell.v2"
 )
@@ -16,6 +15,12 @@ func (b *Bot) Shell() *ishell.Shell {
 		Name: "add-queue",
 		Help: "add a new queue to the bot queues map.: add-queue {string}",
 		Func: b.shellAddQueue,
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "queues",
+		Help: "Print all the queues in the bot map",
+		Func: b.shellPrint,
 	})
 
 	shell.AddCmd(&ishell.Cmd{
@@ -32,19 +37,29 @@ func (b *Bot) shellExit(c *ishell.Context) {
 	os.Exit(0)
 }
 
+func (b *Bot) shellPrint(c *ishell.Context) {
+	keys := b.queueMap.Keys()
+	for i := range keys {
+		q, ok := b.queueMap.Load(keys[i])
+		if !ok {
+			continue
+		}
+
+		log.With(log.F{
+			"Name":      q.Name.String,
+			"Consuming": q.Consuming.Bool,
+		}).Info("Queue")
+	}
+}
+
 func (b *Bot) shellAddQueue(c *ishell.Context) {
 	arguments := c.Args
 	if len(arguments) != 1 {
 		log.Warn("It should add just one queue each time")
+		return
 	}
 
 	queueName := arguments[0]
-	queue := rabbit.NewQueue(queueName)
-
-	if err := queue.Declare(b.channel); err != nil {
-		log.WithError(err).Error("error on declare queue")
-		return
-	}
 	if err := b.AddQueueToConsume(queueName); err != nil {
 		log.WithError(err).Error("error on add queue")
 		return
